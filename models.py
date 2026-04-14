a0c80909555e11b591f4bd981a8579ea82c56678
Jeremy Junior <jrjn71780@gmail.com>
2026-04-14 15:30:24 +0300

    Added Class Exercises

--- models.py
+++ models.py
@@ -21,3 +21,28 @@ class Workout(db.Model):
 
     def __repr__(self):
         return f"<Workout {self.title}>"
+
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    muscle_group = db.Column(db.String(50), nullable=False)
+    equipment = db.Column(db.String(100))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # relationship
+    workout_exercises = db.relationship(
+        "WorkoutExercise",
+        back_populates="exercise",
+        cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<Exercise {self.name}>"
+
+
+
