f5ee543f7ad5ccb9b676e1060ebb45f59724bed0
Jeremy Junior <jrjn71780@gmail.com>
2026-04-14 15:34:49 +0300

    Added configaration and imports

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask , jsonify
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from models import db
+import os
 
 app = Flask(__name__)
-migrate = Migrate(app)
 
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
 @app.route("/")
 def index():
     return jsonify({"message" : "Your Workout app is running"})
