db77cd50c225a830bcd82a8ae44031f19d233432
Jeremy Junior <jrjn71780@gmail.com>
2026-04-14 15:29:13 +0300

    added class Workout

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask , jsonify
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+migrate = Migrate(app)
 
 @app.route("/")
 def index():
--- models.py
+++ models.py
@@ -1,6 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 db = SQLAlchemy()
 
+class Workout(db.Model):
+    __tablename__ = "workouts"
 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # relationship
+    workout_exercises = db.relationship(
+        "WorkoutExercise",
+        back_populates="workout",
+        cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"<Workout {self.title}>"
