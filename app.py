04c8c7d28f4e5bfd3d0335d6c58e84c4e3314300
Jeremy Junior <jrjn71780@gmail.com>
2026-04-14 15:49:49 +0300

    create workout route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask , jsonify
+from flask import Flask , jsonify , request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from models import db, Workout, Exercise, WorkoutExercise
 import os
 
 app = Flask(__name__)
@@ -15,9 +15,28 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+
 @app.route("/")
 def index():
     return jsonify({"message" : "Your Workout app is running"})
+
+# create workout
+@app.route("/workouts", methods=["POST"])
+def create_workout():
+    data = request.get_json()
+
+    workout = Workout(
+        title=data.get("title"),
+        description=data.get("description")
+    )
+
+    db.session.add(workout)
+    db.session.commit()
+
+    return jsonify({"message": "Workout created", "id": workout.id}), 201
+
+
 
 if __name__ == "__main__":
     app.run(debug=True , port=5000)
